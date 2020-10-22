eb9d1cf403fd19ce3c0821e8511ee42fddbf9540
don Pablo <don@obeyi.com>
2020-10-22 17:56:05 +0200

    Remove extra files

--- conver.py
+++
@@ -1,6 +0,0 @@
-import swagger2json
-import json
-swagger= Swagger(json_url='codegen/in/swapi_open.json')
-swagger.parse()
-for items in swagger.result:
-   print(json.dumps(items, sort_keys=True, indent=4, separators=(',', ': ')).encode('utf-8').decode('unicode_escape'))
