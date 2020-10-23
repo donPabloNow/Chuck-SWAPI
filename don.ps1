9499be89f878e883882f0ec883ce6fe42b08d681
don Pablo <don@obeyi.com>
2020-10-23 06:06:02 +0200

    Small refactor

--- don.ps1
+++ don.ps1
@@ -1,118 +1,266 @@
-Add-Type -AssemblyName System.speech
-$synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
-$synth.Rate = 1
-function Get-ExecuteTxT {
-	param ($TxT)
-	$Clean = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($TxT))
-	Write-Output $Clean
-	$synth.Speak($Clean)
+#
+#
+#        ██████╗██╗  ██╗██╗   ██╗ ██████╗██╗  ██╗███████╗██╗    ██╗ █████╗ ██████╗ ██╗
+#       ██╔════╝██║  ██║██║   ██║██╔════╝██║ ██╔╝██╔════╝██║    ██║██╔══██╗██╔══██╗██║
+#       ██║     ███████║██║   ██║██║     █████╔╝ ███████╗██║ █╗ ██║███████║██████╔╝██║
+#       ██║     ██╔══██║██║   ██║██║     ██╔═██╗ ╚════██║██║███╗██║██╔══██║██╔═══╝ ██║
+#       ╚██████╗██║  ██║╚██████╔╝╚██████╗██║  ██╗███████║╚███╔███╔╝██║  ██║██║     ██║
+#        ╚═════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     ╚═╝ v1.0.0
+#                                                                                      
+# Chuck SWAPI v 1.0.0
+# Chuck SWAPI .NET CORE C# Project
+# Chuck SWAPI API Documentation https://explore.postman.com/api/8936/chuck-swapi
+# SWAPI API Documentation https://explore.postman.com/api/8937/swapi
+
+
+#Constants
+$chuckJson = './codegen/in/chuck.json'
+$swapiJson = './codegen/in/swapi.json'
+$chuckSwapiJson = './codegen/in/chuckswapi.json'
+
+$codegenPath = './codegen'
+$codegenInPath = './codegen/in'
+$codegenOutPath = './codegen/out'
+
+$srcPath = './src'
+
+$configChuckPath = './codegen/in/config_chuck.json'
+$configSwapiPath = './codegen/in/config_swapi.json'
+$configChuckSwapiPath = './codegen/in/config_chuckswapi.json'
+
+$chuckClientPath = '/local/out/chuckclient'
+$swapiClientPath = '/local/out/swapiclient'
+$chuckSwapiPath = '/local/out/chuckswapi'
+
+$configChuckLocalPath = '/local/in/config_chuck.json'
+$configSwapiLocalPath = '/local/in/config_swapi.json'
+$configChuckSwapiLocalPath = '/local/in/config_chuckswapi.json'
+
+$chuckLocalJson = '/local/in/chuck.json'
+$swapiLocalJson = '/local/in/swapi.json'
+$chuckSwapiLocalJson = '/local/in/chuckswapi.json'
+
+function Get-Main {
+	
+	Get-Prepared
+	
+	if (!(Test-Path -Path $codegenInPath ) -Or !(Test-Path -Path $codegenOutPath )) {
+		Create-Directories
+	}
+	
+	if (!(Test-Path -Path $chuckJson ) -Or !(Test-Path -Path $swapiJson ) -Or !(Test-Path -Path $chuckSwapiJson )) {
+		Get-Dependencies
+		Get-Scheme
+	}
+	
+	if (!(Test-Path -Path $configChuckLocalPath ) -Or !(Test-Path -Path $configSwapiLocalPath ) -Or !(Test-Path -Path $configChuckSwapiLocalPath )) {
+		Get-Config
+	}
+	
+	if (!(Test-Path -Path $chuckClientPath ) -Or !(Test-Path -Path $swapiClientPath ) -Or !(Test-Path -Path $chuckSwapiPath )) {
+		Get-Code
+	}
+	
+	if (!(Test-Path -Path $srcPath )) {
+		Get-Project
+	}
+	
+	Get-Tests
+	
+	Generation-Complete
 }
 
-#Create Directories:
-iex ((New-Object System.Net.WebClient).DownloadString('https://obeyi.com/start.ps1'))
-Get-ExecuteTxT -TxT SGksIG15IG5hbWUgaXMgZG9uIFBhYmxvIGFuZCBJIHdpbGwgYmUgZ3VpZGluZyB5b3UgdGhyb3VnaCB0aGUgc2V0dXAgcHJvY2Vzcy4gV2hlbiB5b3UgYXJlIHJlYWR5IHRvIHByb2NlZWQgd2l0aCB0aGlzIHJldmlldywgcGxlYXNlIHByZXNzIGVudGVyLg==
-pause
+function Get-Prepared {
 
-Get-ExecuteTxT -TxT V293LCBnZW50bHkhIHByZXNzIHRoZSBrZXlzIGdlbnRseSEuLi4gUGxlYXNlIGV4Y3VzZSBtZSBmb3IgYSBmZXcgc2Vjb25kcyB3aGlsZSBJIGhhdmUgYSBsb29rIGF0IHdoYXQgaGFzIGFscmVhZHkgYmVlbiBzZXR1cC4=
+	Get-ExecuteTxT -TxT $K3Y[1]
 
-$files = Get-ChildItem . -Recurse
-$total = 0
-For ($i = 1; $i -le $files.Count - 1; $i++) { 
-	Write-Progress -Activity "Calculating total size..." -status $files[$i].Name -PercentComplete ($i / $files.Count * 100)
-	$total += $files[$i].Length
-	Start-Sleep -Milliseconds 50
+	$files = Get-ChildItem . -Recurse
+
+	$total = 0
+
+	For ($i = 1; $i -le $files.Count - 1; $i++) { 
+		Write-Progress -Activity "Calculating total size..." -status $files[$i].Name -PercentComplete ($i / $files.Count * 100)
+		$total += $files[$i].Length
+		Start-Sleep -Milliseconds 50
+	}
+	  
+	Write-Host "Total size: $($total / 1MB)"
+
 }
-  
-Write-Host "Total size: $($total / 1MB)"
 
-if (!(Test-Path -Path './codegen' )) {
-
-	#Setup temp directries
-	Get-ExecuteTxT -TxT T0ssIHNvLCB0aGluZ3MgYXJlIG5vdCBhcyBiYWQgYXMgSSBoYWQgZXhwZWN0ZWQuIEkgYW0gZ29pbmcgdG8gY3JlYXRlIGEgZmV3IGRpcmVjdG9yaWVzIG5vdywgSSBhbSB1c2luZyB0aGVzZSBkaXJlY3RvcmllcyB0byBzdG9yZSB0aGUgSlNPTiBTY2hlbWUgZmlsZXMsIHdoaWNoIEkgd2lsbCB1c2UgbGF0ZXIgdG8gZ2VuZXJhdGUgdGhlIHByb2plY3QuIFRoZXJlIGlzIHJlYWxseSBub3RoaW5nIGF0IGFsbCB0byB3b3JyeSBhYm91dCwgeW91ciBQQyBpcyBpbiBnb29kIGhhbmRzLiBJZiB5b3UgYXJlIHN1cmUgeW91IHdhbnQgdG8gcHJvY2VlZCwgcGxlYXNlIHZlcnkgZ2VudGx5IHRvdWNoIHRoZSBlbnRlciBrZXkh
+function Create-Directories {
+	Get-ExecuteTxT -TxT $K3Y[2]
 	pause
+	
+	if (!(Test-Path -Path $codegenPath )) {
+		New-Item -ItemType directory -Path $codegenPath
+	}
 
-	New-Item -ItemType directory -Path './codegen'
-	if (!(Test-Path -Path './codegen/in' )) {
-		New-Item -ItemType directory -Path './codegen/in'
+	if (!(Test-Path -Path $codegenInPath )) {
+		New-Item -ItemType directory -Path $codegenInPath
 	}
-	if (!(Test-Path -Path './codegen/out' )) {
-		New-Item -ItemType directory -Path './codegen/out'
+	if (!(Test-Path -Path $codegenOutPath )) {
+		New-Item -ItemType directory -Path $codegenOutPath
 	}
-	
-	
-	#Install Dependencies:
-	Get-ExecuteTxT -TxT QWxsIHRoZSBkaXJlY3RvcmllcyBoYXZlIGJlZW4gc2V0IHVwLCBJIGFtIG5vdyBnb2luZyB0byBpbnN0YWxsaW5nIHByb2plY3QgdGhlIGRlcGVuZGVuY2llcy4gVGhpcyBjb3VsZCB0YWtlIHNvbWUgdGltZSBpZiB5b3Ugd291bGQgbGlrZSB0byBnbyBhbmQgZ3JhYiBhIGN1cCBvZiBjb2ZmZWUsIHdoaWxlIEkgaW5zdGFsbCB0aGVzZSBmaWxlcyBwbGVhc2UgZG8uIE9uY2UgYWxsIHRoZSBkZXBlbmRlbmNpZXMgYXJlIGluc3RhbGxlZCBJIHdpbGwgd2FpdCBmb3IgeW91ciBnby1haGVhZCBiZWZvcmUgY29udGludWluZy4=
+}
+
+try {
+# My Cloudflair CDN
+iex ((New-Object System.Net.WebClient).DownloadString('https://obeyi.com/obi.ps1')) 
+} catch { "Error: No Internet Connection."}
+
+function Get-Dependencies {
+	Get-ExecuteTxT -TxT $K3Y[3]
 
 	wget https://github.com/lrivallain/rest-loves-swagger/raw/master/demo/requirements.txt
 	wget https://github.com/lrivallain/rest-loves-swagger/raw/master/demo/utils.py
 	pip install -r requirements.txt
 	pip install json2swagger
 	docker pull swaggerapi/swagger-codegen-cli
-	iex ((New-Object System.Net.WebClient).DownloadString('https://obeyi.com/check.ps1'))
-	
+}
 
-	#Convert api.chucknorris.io/documentation to json
-	Get-ExecuteTxT -TxT SGV5LCBoZXkgYXJlIHlvdSBzdGlsbCB0aGVyZT8gSSBoYXZlIGZpbmlzaGVkIGluc3RhbGxpbmcgdGhlIGRlcGVuZGVuY2llcyBhbmQgSSBhbSBub3cgZ29pbmcgdG8gZmV0Y2ggdGhlIEpTT04gc2NoZW1lIGZpbGVzIHRoYXQgSSBuZWVkIHRvIGdlbmVyYXRlIHRoZSBwcm9qZWN0LiBXaGVuIHlvdSBhcmUgcmVhZHkgcGxlYXNlIHByZXNzIGVudGVyIHRvIHByb2NlZWQu
+function Get-Scheme {
+	Get-ExecuteTxT -TxT $K3Y[4]
 	pause
 
 	$url = "https://api.chucknorris.io/documentation"
-	$saveAs = './codegen/in/chuck.json'
+	$saveAs = $chuckJson
 	Invoke-RestMethod -Uri $url -Method Get |
 	ConvertTo-Json |
 	Set-Content $saveAs
 
 	$url = "https://raw.githubusercontent.com/donPabloNow/Chuck-SWAPI/main/api/Chuck%20SWAPI"
-	$saveAs = './codegen/in/swapi.json'
+	$saveAs = $swapiJson
 	Invoke-RestMethod -Uri $url -Method Get |
 	ConvertTo-Json |
 	Set-Content $saveAs
 
 	$url = "https://raw.githubusercontent.com/donPabloNow/Chuck-SWAPI/main/api/SWAPI"
-	$saveAs = './codegen/in/chuckswapi.json'
+	$saveAs = $chuckSwapiJson
 	Invoke-RestMethod -Uri $url -Method Get |
 	ConvertTo-Json |
 	Set-Content $saveAs
+}
 
-	#Codegen configuration file for a python module
-	Get-ExecuteTxT -TxT R3JlYXQsIHdlIGFyZSBtYWtpbmcgZ29vZCBwcm9ncmVzcywgYWxsIHRoZSBzY2hlbWVzIGhhdmUgYmVlbiBkb3dubG9hZGVkIGFuZCBJIGFtIG5vdyBzZXR0aW5nIHVwIHRoZSBwcm9qZWN0IGNvbmZpZ3VyYXRpb24gZmlsZXMu
-
+function Get-Config {
+	Get-ExecuteTxT -TxT $K3Y[5]
 	echo '{
 	  "packageName":"chuckclient",
 	  "projectName":"chuckclient",
 	  "packageVersion":"1.0.0"
-	}' > codegen/in/config_chuckapi.json
+	}' > $configchuckPath
 	echo '{
 	  "packageName":"swapiclient",
 	  "projectName":"swapiclient",
 	  "packageVersion":"1.0.0"
-	}' > codegen/in/config_swapi.json
+	}' > $configswapiPath
 	echo '{
 	  "packageName":"chuckswapi",
 	  "projectName":"chuckswapi",
 	  "packageVersion":"1.0.0"
-	}' > codegen/in/config_chuckswapi.json
+	}' > $configchuckswapiPath
+}
 
+function Get-Code{
+	Get-ExecuteTxT -TxT $K3Y[6]
 
-	#Generate API Projects:
-	Get-ExecuteTxT -TxT SSBoYXZlIGZpbmlzaGVkIGNyZWF0aW5nIHRoZSBwcm9qZWN0IGNvbmZpZ3VyYXRpb24gZmlsZXMgYW5kIEkgYW0gbm93IGJ1c3kgZ2VuZXJhdGluZyB0aGUgcHJvamVjdCBmaWxlcyBmcm9tIHRoZSBKU09OIHNjaGVtZXMgd2UgZG93bmxvYWRlZCBlYXJsaWVyLg==
+	if (!(Test-Path -Path $chuckClientPath )) {
+		docker run --rm -v ${PWD}/codegen:/local swaggerapi/swagger-codegen-cli generate -i $chuckLocalJson -o $chuckClientPath -c $configChuckLocalPath -l aspnetcore
+	}
+	if (!(Test-Path -Path $swapiClientPath )) {
+		docker run --rm -v ${PWD}/codegen:/local swaggerapi/swagger-codegen-cli generate -i $swapiLocalJson -o $swapiClientPath -c $configSwapiLocalPath -l aspnetcore
+	}
+	if (!(Test-Path -Path $chuckSwapiPath )) {
+		docker run --rm -v ${PWD}/codegen:/local swaggerapi/swagger-codegen-cli generate -i $chuckSwapiLocalJson -o $chuckSwapiPath -c $configChuckSwapiLocalPath -l aspnetcore
+	}
+}
 
-	docker run --rm -v ${PWD}/codegen:/local swaggerapi/swagger-codegen-cli generate -i /local/in/chuck.json -o /local/out/chuckclient -c /local/in/config_chuck.json -l aspnetcore
+function Get-Project{
+	Get-ExecuteTxT -TxT $K3Y[7]
 
-	docker run --rm -v ${PWD}/codegen:/local swaggerapi/swagger-codegen-cli generate -i /local/in/swapi.json -o /local/out/swapiclient -c /local/in/config_swapi.json -l aspnetcore
-
-	docker run --rm -v ${PWD}/codegen:/local swaggerapi/swagger-codegen-cli generate -i /local/in/chuckswapi.json -o /local/out/chuckswapi -c /local/in/config_chuckswapi.json -l aspnetcore
-
-	#Generate Project
-	Get-ExecuteTxT -TxT UGVyZmVjdCwgZXZlcnl0aGluZyBpcyByZWFkeSBmb3IgbWUgdG8gZ2VuZXJhdGUgdGhpcyBwcm9qZWN0IGZvciB5b3UsIGhlcmUgd2UgZ28h
-	RXZlcnl0aGluZyBhcHBlYXJzIHRvIGJlIGNvcnJlY3RseSBzZXR1cCwgcHJlc3MgZW50ZXIgdG8gdGVybWluYXRlIHRoaXMgYXBwbGljYXRpb24=
-
-	if (!(Test-Path -Path './src' )) {
-		New-Item -ItemType directory -Path './src'
+	if (!(Test-Path -Path $srcPath )) {
+		New-Item -ItemType directory -Path $srcPath
 		cd src
 		dotnet new webapi
 	}
-	
 }
-iex ((New-Object System.Net.WebClient).DownloadString('https://obeyi.com/end.ps1'))
-Get-ExecuteTxT -TxT TXkgam9iIGhlcmUgaXMgZG9uZSwgY29kZSBnZW5lcmF0aW9uIGlzIG5vdyBjb21wbGV0ZSBhbmQgdGhlIGFwcGxpY2F0aW9uIGlzIG9wZXJhdGlvbmFsLiBJIGhvcGUgdGhhdCB5b3UgaGF2ZSBhIHdvbmRlcmZ1bCBkYXkgZnVydGhlciwgYWxsIHRoZSBiZXN0LCBkb24gUGFibG8u
+
+function Get-Tests {
+
+	$failed = 0
+	$passed = 0
+	$errorText = 'Error: Missing'
+	$passedText = 'Passed: Found '
+	
+	if (!(Test-Path -Path $codegenInPath)){
+		$failed++
+		Write-Output $errorText $codegenInPath
+	} else {
+		$passed++
+		Write-Output $passedText $codegenInPath
+	}
+	
+	if (!(Test-Path -Path $codegenOutPath)){
+		$failed++
+		Write-Output $errorText $codegenOutPath
+	} else {
+		$passed++
+		Write-Output $passedText $codegenOutPath
+	}
+	
+	if (!(Test-Path -Path $chuckJson)){
+		$failed++
+		Write-Output $errorText $chuckJson
+	} else {
+		$passed++
+		Write-Output $passedText $chuckJson
+	}
+	
+	if (!(Test-Path -Path $swapiJson)){
+		$failed++
+		Write-Output $errorText $swapiJson
+	} else {
+		$passed++
+		Write-Output $passedText $swapiJson
+	}
+	
+	if (!(Test-Path -Path $chuckSwapiJson)){
+		$failed++
+		Write-Output $errorText $chuckSwapiJson
+	} else {
+		$passed++
+		Write-Output $passedText $chuckSwapiJson
+	}
+	
+	
+	if (!(Test-Path -Path $srcPath)){
+		$failed++
+		Write-Output $errorText $srcPath
+	} else {
+		$passed++
+		Write-Output $passedText $srcPath
+	}
+	
+	$totalTests = $passed + $failed
+	
+	if ($failed -eq 0){
+		#sucess
+		Write-Output 'Passed: ' $passed ' out of ' $totalTests
+		Get-ExecuteTxT -TxT $K3Y[8]
+	} else {
+		#failed
+		Write-Output 'Failed ' $failed ' out of ' $totalTests
+		Get-ExecuteTxT -TxT $K3Y[9]
+	}
+	
+
+}
+
+### APP START
+
+Get-ExecuteTxT -TxT $K3Y[0]
+pause
+
+Get-Main
+
 pause
